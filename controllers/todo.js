2d02075ecc60cc6bd249ca442ad829f9c3bf799f
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-05-19 20:50:03 +0300

    bthngs

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,14 +1,14 @@
-const handleTodo = (req, res, db) => {
-    {
-        db.select("*").from("task").then(data => {
-            res.render("todo", { todos: data });
-            console.log(data);
-        }).catch(err => res.status(400).json(err));
-    }
-}
-module.exports = {
-    handleTodo: handleTodo
-};
+// const handleTodo = (req, res, db) => {
+//     {
+//         db.select("*").from("task").then(data => {
+//             res.render("todo", { todos: data });
+//             console.log(data);
+//         }).catch(err => res.status(400).json(err));
+//     }
+// }
+// module.exports = {
+//     handleTodo: handleTodo
+// };
 
 
 
