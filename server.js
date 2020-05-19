1cd8698b9790a12d4bf975735c5146a71b2e1722
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-05-19 18:35:59 +0300

    server edit

--- server.js
+++ server.js
@@ -30,8 +30,6 @@ app.get('/', (req, res) => { res.send('It is working') })
 app.post('/signin', signin.handleSignIn(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
