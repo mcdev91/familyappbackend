052ae3bce9f40f311e68c2e20bca000e0a897334
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-05-20 20:25:47 +0300

    f

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,10 +1,6 @@
 const handleTodos = (req, res, db) => {
-    try {
         const allTodos = db.select('*').from('todo')
         console.log(res.json(allTodos));
-    } catch (error) {
-        console.error(error.message);
-    }
 }
 
 module.exports = {
