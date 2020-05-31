695eaa37abb9dcc9a3597a9360cd0eeeb1541286
martynasdev <43806057+martynasdev@users.noreply.github.com>
2020-05-31 10:57:37 +0300

    fixing

--- server.js
+++ server.js
@@ -33,8 +33,9 @@ app.get('/', (req, res) => { res.send('It is working') })
 app.post('/signin', signin.handleSignIn(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-//
-app.get('/todos', (req, res) => { todos.handleTodos(req, res, db) })
+
+//__________for todo
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
